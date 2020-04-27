712e70a05f6960cdcd307a180f026287b77d3f95
Max Power <dbroncosfan@gmail.com>
2020-04-27 15:57:06 -0600

    heroku

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require("express");
-
 const app = express();
+const path = require("path");
 const PORT = process.env.PORT || 3000;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')));
 
 // Start the API server
 app.listen(PORT, function() {
