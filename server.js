cf2b87f0c10eae87210d9552133b40a8c60485cf
Max Power <dbroncosfan@gmail.com>
2020-04-27 15:35:00 -0600

    heroku deployment

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-var express = require('express')
-var path = require('path')
+const express = require("express");
 
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-var app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-var port = process.env.PORT || 8000
-app.listen(port)
-console.log('server started ' + port)
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
