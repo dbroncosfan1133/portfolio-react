20027430f846965ec24a4ac610a9ed3447926d99
Max Power <dbroncosfan@gmail.com>
2020-04-27 17:03:16 -0600

    heroku

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3000;
 
+const publicPath = path.join(__dirname, "..", "portfolio-react/public")
+app.use(express.static(publicPath));
+
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,7 +15,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('*', (req, res) => res.sendFile(path.join(publicPath, 'index.html')));
 
 // Start the API server
 app.listen(PORT, function() {
