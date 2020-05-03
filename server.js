f13bb912c859ab40f6561b62c5e4dd897bbc1921
Max Power <dbroncosfan@gmail.com>
2020-05-02 23:30:07 -0600

    Working app with server file, moved public files to client folder

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
 const express = require("express");
-const app = express();
-const path = require("path");
-const PORT = process.env.PORT || 3000;
 
-const publicPath = path.join(__dirname, "..", "portfolio-react/public")
-app.use(express.static(publicPath));
+// const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
-app.get('*', (req, res) => res.sendFile(path.join(publicPath, 'index.html')));
+// Add routes, both API and view
+// app.use(routes);
 
 // Start the API server
 app.listen(PORT, function() {
